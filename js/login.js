4ff5ae05a8708e304c16bee280ae19f6ee0ac613
Mike McKenna <mikemckenna47@gmail.com>
2017-09-30 23:15:20 +0100

    Authentication

--- js/login.js
+++ js/login.js
@@ -1,30 +1,20 @@
 AWS.config.region='us-west-2';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId:'us-west-2:7f428de1-156c-410d-a485-b85f6e5c5d83'});
 
-//AWS.config.region = '<REGION>';
-//AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//  IdentityPoolId: '<IDENTITY_POOL_ID>'
-//});
-
-
-
 var lambda = new AWS.Lambda();
 
-function signup() {
+function login() {
 
   var result = document.getElementById('result');
   var email = document.getElementById('email');
   var password = document.getElementById('password');
-  var verifyPassword = document.getElementById('verify-password');
 
-  result.innerHTML = 'Sign Up...';
+  result.innerHTML = 'Login...';
 
-	if (email.value == null || email.value == '') {
-  	result.innerHTML = 'Please specify your email address.';
+  if (email.value == null || email.value == '') {
+    result.innerHTML = 'Please specify your email address.';
   } else if (password.value == null || password.value == '') {
     result.innerHTML = 'Please specify a password.';
-  } else if (password.value != verifyPassword.value) {
-    result.innerHTML = 'Passwords are <b>different</b>, please check.';
   } else {
 
     var input = {
@@ -33,25 +23,35 @@ function signup() {
     };
 
     lambda.invoke({
-      FunctionName: 'createUser',
+      FunctionName: 'login',
       Payload: JSON.stringify(input)
     }, function(err, data) {
       if (err) console.log(err, err.stack);
       else {
         var output = JSON.parse(data.Payload);
-        if (output.created) {
-          result.innerHTML = 'User ' + input.email + ' created. Please check your email to validate the user and enable login.';
+        if (!output.login) {
+          result.innerHTML = '<b>Not</b> logged in';
         } else {
-          result.innerHTML = 'User <b>not</b> created.';
+          result.innerHTML = 'Logged in with IdentityId: ' + output.identityId + '<br>';
+
+          var creds = AWS.config.credentials;
+          creds.params.IdentityId = output.identityId;
+          creds.params.Logins = {
+            'cognito-identity.amazonaws.com': output.token
+          };
+          creds.expired = true;
+
+          // Do something with the authenticated role
+
         }
       }
     });
 
-  }
+	}
 }
 
-var form = document.getElementById('signup-form');
+var form = document.getElementById('login-form');
 form.addEventListener('submit', function(evt) {
   evt.preventDefault();
-  signup();
+  login();
 });
